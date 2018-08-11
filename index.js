163cd2820db08732215160c484925756b83942d2
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 19:21:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,36 +3,44 @@ function add(a,b)
   var num = a+b;
   return num;
 }
-
+add(1,2);
 function subtract(a,b)
 {
   var num = a-b;
   return num;
 }
+subtract(1,2);
 function multiply(a,b)
 {
   var num = a*b;
   return num;
 }
+multiply(1,2);
 function divide(a,b)
 {
   var num = a/b;
   return num;
 }
+divide(1,2);
 function inc(n)
 {
   n++;
   return n;
 }
+inc(1);
 function dec(n)
 {
   n--;
   return n;
 }
-function makeInt(n)
+dec(2);
+function makeInt(n, x)
 {
-  return parseInt(n);
+  return parseInt(n, x);
 }
-function makeInt(n){
-  return parseInt(n,10);
-}
+makeInt(1.2,1);
+makeInt(1.2, 10);
+makeInt('no', 1);
+
+
+
