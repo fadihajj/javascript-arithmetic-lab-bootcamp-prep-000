802b8f71ddc445b44adc2b66eb3e15c3355bad19
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 19:17:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function multiply(a,b)
   var num = a*b;
   return num;
 }
+function divide(a,b)
+{
+  var num = a/b;
+  return num;
+}
+function inc(n)
+{
+  n++;
+  return n;
+}
+function dec(n)
+{
+  n--;
+  return n;
+}
